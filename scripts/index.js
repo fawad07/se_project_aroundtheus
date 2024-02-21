b6d63b6b1df967ae78709e79959bfd5ac8312f1e
fawad07 <muhammad.f.aleem@gmail.com>
2024-02-21 04:59:33 -0800

    fix/ refractored names and add card modal

--- scripts/index.js
+++ scripts/index.js
@@ -29,39 +29,53 @@ const initialCards = [
 /*                 ELEMENTS                                 */
 /*----------------------------------------------------------*/
 const profileEditButton = document.querySelector("#js-profile-edit-button");
-console.log(profileEditButton);		//debugging statement 
+//console.log(profileEditButton);		//debugging statement 
 
 const profileEditModal = document.querySelector("#js-profile-edit-modal");
-console.log(profileEditModal);		//debugging sttement
+//console.log(profileEditModal);		//debugging sttement
 
-const profileCloseModal = document.querySelector("#js-profile-close-modal");
-console.log(profileCloseModal);		//debugging statement 
+const profileCloseModal = profileEditModal.querySelector("#js-profile-close-modal");
+//console.log(profileCloseModal);		//debugging statement 
 
 const profileTitle = document.querySelector("#js-profile-title");
-console.log(profileTitle.textContent);			//debugging statement
+//console.log(profileTitle.textContent);			//debugging statement
 
 const profileDescription = document.querySelector("#js-profile-description");
-console.log(profileDescription.textContent);	//debugging statemente
+//console.log(profileDescription.textContent);	//debugging statemente
 
 const profileTitleInput = document.querySelector("#js-profile-title-input");
-console.log(profileTitleInput.value);					//debugging statement
+//console.log(profileTitleInput.value);					//debugging statement
 
 const profileDescriptionInput = document.querySelector("#js-profile-description-input");
-console.log(profileDescriptionInput.value);				//debugging statement
+//console.log(profileDescriptionInput.value);				//debugging statement
 
-const profileEditForm = profileEditModal.querySelector("#js-modal-form");
-console.log(profileEditForm);							//debugging statement
+const profileEditForm = profileEditModal.querySelector("#js-modal-edit-form");
+//console.log(profileEditForm);							//debugging statement
 
 const cardTemplate = document.querySelector("#js-card-template").content.firstElementChild;
-console.log(cardTemplate);								//debugging statement 
+//console.log(cardTemplate);								//debugging statement 
 
 const cardListElement = document.querySelector("#js-card__list");
+
+const profileAddCardButton = document.querySelector("#js-profile-add-button");			//button for adding card
+//console.log(profileAddCardButton);
 
 /*----------------------------------------------------------*/
 /*               			FUNCTIONS		                */
 /*----------------------------------------------------------*/
-function closePopUp()
+
+//open modal when edit button clicked/modal
+function profileEditButtonClicked(modal)
 {
+	console.log("profile editButton/add Clicked!! and this function name needs to be Changed");	//debugging statement
+	profileEditModal.classList.add("modal_opened");
+	
+}//end func 
+
+//close edit profile pop up/modal
+function closePopUp(modal)
+{
+	console.log("profile CloseButton Clicked in pop up!!");	//debugging statement
 	profileEditModal.classList.remove("modal_opened");
 }//end func 
 
@@ -69,14 +83,14 @@ function getCardElement(cardData)
 {
 	//clone the template element with all its content and store it in a cardElement variable
 	const cardElement = cardTemplate.cloneNode(true);
-	console.log(cardElement);		//debuging statement 
+	//console.log(cardElement);		//debuging statement 
 	
 	//access the card title and image and store them in variables
 	const cardImageElement = cardElement.querySelector("#js-card__image");
-	console.log(cardImageElement);		//debugging statment
+	//console.log(cardImageElement);		//debugging statment
 	
 	const cardTitleElement = cardElement.querySelector("#js-card__title");
-	console.log(cardTitleElement);		//debugging statement 
+	//console.log(cardTitleElement);		//debugging statement 
 	
 	//set the path to the image to the link field of the object
 	cardImageElement.src = cardData.link;
@@ -85,7 +99,7 @@ function getCardElement(cardData)
 	
 	//set the card title to the name field of the object, too
 	cardTitleElement.textContent = cardData.name;
-	console.log(cardTitleElement);		//debugging statement 
+	//console.log(cardTitleElement);		//debugging statement 
 	
 	//return the ready HTML element with the filled-in data
 	return cardElement;
@@ -98,33 +112,38 @@ function getCardElement(cardData)
 
 //profile edit button clicked -- pop up opened
 profileEditButton.addEventListener("click", () => {
-	console.log("profile editButton Clicked!!");	//debugging statement
-	
 	profileTitleInput.value = profileTitle.textContent;				//pre-filled form with the profile title 
 	profileDescriptionInput.value = profileDescription.textContent; //pre-filled form with the profile description 
-	profileEditModal.classList.add("modal_opened");
+	profileEditButtonClicked(profileEditModal)
+	
 });
 
 //close button  -- close pop-up 
-profileCloseModal.addEventListener("click", () => {
-	console.log("profile CloseButton Clicked in pop up!!");	//debugging statement
-	closePopUp();
-	//profileEditModal.classList.remove("modal_opened");
-});
+profileCloseModal.addEventListener("click", () => closePopUp(profileCloseModal));
 
-
+//profile save button clicked 
 profileEditForm.addEventListener("submit", (event) => {
 	event.preventDefault();
 	console.log("Save button clicked");
 	profileTitle.textContent = profileTitleInput.value;			//form text content goes to profile value	
 	profileDescription.textContent = profileDescriptionInput.value;		//form text content goes to profile value
 	closePopUp();
-	//profileEditModal.classList.remove("modal_opened");
+	});
+
+//profile add Card button clicked
+profileAddCardButton.addEventListener("click", () => profileEditButtonClicked(profileAddCardButton));
+/* {
+	console.log("Profile add button clicked");		//debugging statement
+	profileEditButtonClicked(profileAddCardButton);
 });
+*/
 
 /************************************************************************/
 
+//TAsk 1: Render Card with forEach() instead of for  loop
 initialCards.forEach((cardData) => {
 	const cardElement = getCardElement(cardData);
 	cardListElement.append(cardElement);
 });
+
+//Task 2: Form for adding a card
