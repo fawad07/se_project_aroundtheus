1541bdf7415aa91bdb875e8b29b1f67b4995a22e
fawad07 <muhammad.f.aleem@gmail.com>
2024-02-28 05:38:21 -0800

    fix/ removed debugging statements

--- scripts/index.js
+++ scripts/index.js
@@ -34,41 +34,31 @@ const initialCards = [
 /*----------------------------------------------------------*/
 
 const profileEditButton = document.querySelector("#js-profile-edit-button");
-//console.log(profileEditButton);		//debugging statement 
 
 const profileEditModal = document.querySelector("#js-profile-edit-modal");
-//console.log(profileEditModal);		//debugging sttement
 
 const profileCloseModal = profileEditModal.querySelector("#js-profile-close-modal");
-//console.log(profileCloseModal);		//debugging statement 
 
 const profileTitle = document.querySelector("#js-profile-title");
-//console.log(profileTitle.textContent);			//debugging statement
 
 const profileDescription = document.querySelector("#js-profile-description");
-//console.log(profileDescription.textContent);	//debugging statemente
 
 const profileTitleInput = document.querySelector("#js-profile-title-input");
-//console.log(profileTitleInput.value);					//debugging statement
 
 const profileDescriptionInput = document.querySelector("#js-profile-description-input");
-//console.log(profileDescriptionInput.value);				//debugging statement
 
 const profileEditForm = profileEditModal.querySelector("#js-modal-edit-form");
-//console.log(profileEditForm);							//debugging statement
 
 /*----------------------------------------------------------*/
 /*                Card Template/Element                             */
 /*----------------------------------------------------------*/
 
 const cardTemplate = document.querySelector("#js-card-template").content.firstElementChild;
-//console.log(cardTemplate);								//debugging statement 
 
 const cardListElement = document.querySelector("#js-card__list");
 
 
 const addCardModal = document.querySelector("#js-add-card-modal");		//grab card modal/popup from html
-console.log(addCardModal);		//debugging statement
 
 const addCardForm = addCardModal.querySelector("#js-modal-add-card-form");
 
@@ -81,7 +71,6 @@ const newCardUrlInput = addCardForm.querySelector("#js-add-card-description-inpu
 /*----------------------------------------------------------*/
 
 const profileAddCardButton = document.querySelector("#js-profile-add-button");			//button for adding card
-//console.log(profileAddCardButton);
 
 //add card modal close button clicked
 const addCardmodalCloseButton = addCardModal.querySelector("#js-add-card-close-modal");
@@ -90,7 +79,6 @@ const addCardmodalCloseButton = addCardModal.querySelector("#js-add-card-close-m
 /*                Image Preview.                            */
 /*----------------------------------------------------------*/
 let imagePreviewModal = document.querySelector("#js-image-preview-modal");
-console.log(imagePreviewModal);		//debugging statement
 
 let imageClosePreviewModal = document.querySelector("#js-image-preview-close-modal");
 
@@ -102,9 +90,7 @@ let imageClosePreviewModal = document.querySelector("#js-image-preview-close-mod
 //open modal when edit button clicked/modal
 function openPopUp(modal)
 {
-	console.log("we are in open pop up func");	//debugging statement
-	modal.classList.add("modal_opened");
-	
+	modal.classList.add("modal_opened");	
 }//end func 
 
 //close edit profile pop up/modal
@@ -118,15 +104,12 @@ function getCardElement(cardData)
 {
 	//clone the template element with all its content and store it in a cardElement variable
 	const cardElement = cardTemplate.cloneNode(true);
-	//console.log(cardElement);		//debuging statement 
 	
 	//access the card title and image and store them in variables
 	const cardImageElement = cardElement.querySelector("#js-card__image");
-	//console.log(cardImageElement);		//debugging statment
 	
 	const cardTitleElement = cardElement.querySelector("#js-card__title");
-	//console.log(cardTitleElement);		//debugging statement 
-	
+
 	//set the path to the image to the link field of the object
 	cardImageElement.src = cardData.link;
 	//set the image alt text to the name field of the object
@@ -134,15 +117,12 @@ function getCardElement(cardData)
 	
 	//set the card title to the name field of the object, too
 	cardTitleElement.textContent = cardData.name;
-	//console.log(cardTitleElement);		//debugging statement 
-
 
 /*----------------------------------------------------------*/
 /*      Card image clicked		     				        */
 /*----------------------------------------------------------*/
 
 	//Card Image Preview - event listener
-	//imagePreview(cardImageElement, cardData);
 	
 	cardImageElement.addEventListener("click", () => {
 		//image view on image preview modal
@@ -150,10 +130,7 @@ function getCardElement(cardData)
 		const imageElement = imagePreviewModal.querySelector("#js-card__image");
 		imageElement.src = cardData.link;
 		imageElement.alt = cardData.name;
-		/*
-		imageElement.style.width = "75vw";
-		imageElement.style.height = "75vh";
-		*/
+		
 		//text view on the image view modal		
 		const titleElement = imagePreviewModal.querySelector("#js-image-preview-card-title");
 		titleElement.textContent = cardData.name;
@@ -240,11 +217,6 @@ profileEditForm.addEventListener("submit", (event) => {
 
 //profile add Card button clicked
 profileAddCardButton.addEventListener("click", () => openPopUp(addCardModal));
-/* {
-	console.log("Profile add button clicked");		//debugging statement
-	profileEditButtonClicked(profileAddCardButton);
-});
-*
 
 /*----------------------------------------------------------*/
 /*      profile add (+) card modal close btn clicked        */
@@ -255,10 +227,9 @@ const  addCardButtonForm = addCardModal.querySelector("#js-modal-add-card-form")
 addCardButtonForm.addEventListener("submit", (event) => {
 	event.preventDefault();
 	console.log("save btn cliked in Add card Modal");			//debugging statement 
-	const name = newCardTitleInput.value;			//newCardTitleValue
-	const link = newCardUrlInput.value;				//newCardUrlValue
-	console.log(`${name} + ${link}`);					//debugging statement
-	//console.log(newCardUrlValue);					//debugging statement
+	const name = newCardTitleInput.value;						//newCardTitleValue
+	const link = newCardUrlInput.value;							//newCardUrlValue
+
 	const newCardEl = getCardElement({name, link});	
 	//call renderCArd func
 	renderCard({name, link}, cardListElement);
@@ -278,15 +249,3 @@ imageClosePreviewModal.addEventListener("click", () => closePopUp(imagePreviewMo
 
 //TAsk 1: Render Card with forEach() instead of for  loop
 initialCards.forEach((cardData) => renderCard(cardData,cardListElement));
-/*
-{
-	const cardElement = getCardElement(cardData);
-	cardListElement.append(cardElement);
-});
-*/
-//CARD LIKE btn --> click and change coloe
-
-//4. add event listener for imageEl in getCardElement() for popup view
-//open image-popup --> popup/moda for image in html (3rd)
-//find image element in popup
-//replace src with the link ,alt with card title 
