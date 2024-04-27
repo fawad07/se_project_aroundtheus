171209853a991268d338eaf635efe66f6f374e44
fawad07 <muhammad.f.aleem@gmail.com>
2024-04-27 10:16:44 -0700

    update/ pop class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,4 +2,47 @@ export class Popup {
 	constructor(popUpSelector) {
 		this._selector = docuemnt.querySelector(popUpSelector);
 	} //end contructor
+
+	open() {
+		this._selector.classList.add("modal_opened");
+		document.addEventListener("keydown", handleCloseEscPressDown);
+		this._selector.addEventListener(
+			"mousedown",
+			handleModalCloseMouseClick
+		);
+	} //end func
+
+	//close edit profile pop up/modal
+	close() {
+		this._selector.classList.remove("modal_opened");
+		document.removeEventListener("keydown", handleModalCloseEscPressDown);
+		this._selector.removeEventListener(
+			"mousedown",
+			handleModalCloseMouseClick
+		);
+	} //end func
+
+	/*
+param: takes the event i.e key pressed
+Description: gets the modal_opened class and
+			checks if the esc key pressed only 
+			than close the pop up/modal 
+*/
+	_handleCloseEscPressDown(evt) {
+		if (evt.key === "Escape") {
+			this.close();
+		} //end if
+	} //end func
+
+	/*
+param: takes the event i.e key pressed
+Description: gets the modal_opened class and
+			checks if the esc key pressed only 
+			than close the pop up/modal 
+*/
+	_handleCloseEscPressDown(evt) {
+		if (evt.key === "Escape") {
+			this._close();
+		} //end if
+	} //end func
 } //end class
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,37 @@
 export class Section {
-    constructor({items, renderer}, containerSelector) {
-        this._renderer = renderer;
-        this._renderItems = items;
-        this._selector = documrnt.querySelector(containerSelector);
-    }//end const
-}//end class
+	constructor({ items, renderer }, containerSelector) {
+		this._renderer = renderer;
+		this._renderItems = items;
+		this._selector = documrnt.querySelector(containerSelector);
+	} //end const
+
+	/**
+	 * param: Card data, image and title
+	 * Description: takes the card data and initializes the cards
+	 * return: none
+	 */
+	renderItems(data) {
+		//use this._renderer to render the data into this._element
+	} //end func
+
+	addItems(item) {
+		//take the items and render in into this._element
+	}
+} //end class
+
+
+/**BELOW IS index.JS EXAMPLE 
+ * 
+ * //create class instances
+ * const cardSection = new Section ({
+ *          renderer: (item) => {
+ *                  const eardElement = new Card(item, selector.cardTemplate);
+ *                  cardSection.additems(cardElement.getview())
+ *                  },
+ *        selector: selector.cardSection,
+ *          });
+ * 
+ * //initialize instances
+ * CardSection.renderitems(initialCArds);
+ * 
+ */
