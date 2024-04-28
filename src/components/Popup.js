d80b36a43e8140078253ad17879f1c9883df5dbc
fawad07 <muhammad.f.aleem@gmail.com>
2024-04-28 00:03:57 -0700

    refrator/popup n popupWithImage class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,25 +1,21 @@
 export class Popup {
 	constructor(popUpSelector) {
 		this._selector = docuemnt.querySelector(popUpSelector);
+		this._closeButton = this._selector.querySelector(".modal__close-button");
 	} //end contructor
 
 	open() {
 		this._selector.classList.add("modal_opened");
-		document.addEventListener("keydown", handleCloseEscPressDown);
-		this._selector.addEventListener(
-			"mousedown",
-			handleModalCloseMouseClick
+		document.addEventListener("keydown", this._handleCloseEscPressDown);
+		this._selector.addEventListener("mousedown", this._handleCloseMouseClick
 		);
 	} //end func
 
 	//close edit profile pop up/modal
 	close() {
 		this._selector.classList.remove("modal_opened");
-		document.removeEventListener("keydown", handleModalCloseEscPressDown);
-		this._selector.removeEventListener(
-			"mousedown",
-			handleModalCloseMouseClick
-		);
+		document.removeEventListener("keydown", this._handleCloseEscPressDown);
+		this._selector.removeEventListener("mousedown",this._handleCloseMouseClick);
 	} //end func
 
 	/*
@@ -34,15 +30,18 @@ Description: gets the modal_opened class and
 		} //end if
 	} //end func
 
-	/*
-param: takes the event i.e key pressed
-Description: gets the modal_opened class and
-			checks if the esc key pressed only 
-			than close the pop up/modal 
+/*
+Param:	takes the event i.e mouse click 
+Description: closes pop up/ modal when clicked outside form 
 */
-	_handleCloseEscPressDown(evt) {
-		if (evt.key === "Escape") {
-			this._close();
-		} //end if
-	} //end func
+_handleCloseMouseClick(evt) {
+	if (evt.target === evt.currentTarget) {
+		this.close();
+	} //end if
+} //end func
+
+setEventListeners() {
+	this._closeButton.addEventListener('click', this._handleCloseMouseClick);
+}//end func
+
 } //end class
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,5 +1,17 @@
+import {Popup} from "./Popup.js";
+
 export class PopupWithImage extends Popup {
 	constructor(popupSelector) {
 		super(popupSelector);
+		this._img =  this._popupSelector.querySelector('#js-card__image');
+		this._title =  this._popupElement.querySelector('#js-image-preview-card-title');
 	} //end const
+
+	open(data) {
+        this._img.src = data.link;
+        this._img.alt = data.name;
+        this._title.textContent = data.name;
+        super.open();
+    }
+
 } //end class
