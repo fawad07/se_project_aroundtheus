74b2d4df4d8778119bf4ac5cb5b3faffacae33e7
fawad07 <muhammad.f.aleem@gmail.com>
2024-05-15 14:16:46 -0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,99 +9,7 @@ import { Api } from "../components/api.js";
 import utils from "../Utils/Constants.js";
 import "../pages/index.css";
 
-/*----------------------------------------------------------*/
-/*               			FUNCTIONS		                */
-/*----------------------------------------------------------*/
-
-/**
- * Param: cardData
- * Description:	card Data is used that is passed to the Image form for display
- */
-//Card Image Preview
-function handleImageClick(cardData) {
-	imagePopup.open(cardData);
-} //end func
-
-/**
- * param: data
- * Description:	Takes a data object that is used to create a card with the Card object
- * 						Card Element is created and passed along to the Section object
- * 						Section object visually puts the card on the screen
- */
-//func to make new card
-function createCard(data) {
-	const newCard = new Card(
-		data,
-		utils.htmlIds.cardTemplate,
-		handleImageClick
-	); //pass new info to new card
-	const elementCard = newCard.getCard();
-	sectionCards.addItems(elementCard);
-} //end func
-
-/**
- * Param: settings/configurations
- * Description: all form validation initialize
- */
-function validateForms(opts) {
-	//initialize forms
-	const forms = document.querySelectorAll(utils.config.formSelector);
-	const formArray = Array.from(forms);
-	formArray.forEach((form) => {
-		const validateForm = new FormValidator(opts, form);
-		validateForm.enableValidation();
-	});
-} //end func
-
-/**
- * Param: userData
- * Description:	Sets the change/edit name/title and the description of the profile
- */
-function handleProfileSubmitForm(userData) {
-
-	/*console.log(userData);	//debugging
-	return;*/
-
-	// Call the updateUserInfo method from the Api class
-	api.updateUserInfo(userData.title, userData.description)
-		.then((data) => {
-			userInfo.setUserInfo({
-				name: userData.title,
-				description: userData.description,
-			});
-			profileEditForm.close();
-		})
-		.catch((err) => {
-			console.error(`Error updating new information: ${err}`);
-		});
-
-	/*
-	userInfo.setUserInfo({
-		name: userData.title,
-		description: userData.description,
-	});
-	profileEditForm.close();
-	*/
-} //end func
-
-/**
- * Description:	Adds a new card everytime the + button is clicked
- */
-function handleAddCardSubmitForm() {
-	//add new card i.e. Create Card
-	const name = utils.newCardTitleInput.value; //newCardTitleValue
-	const link = utils.newCardUrlInput.value; //newCardUrlValue
-	const dataCard = { name, link }; //collect new card data and link
-	createCard(dataCard);
-
-	//close addCardPopup
-	addCardForm.close();
-} //end func
-
-/********************INSTANISATIONS BELOW******************** */
-
-//ALL FORMS VALIDATIONS
-validateForms(utils.config);
+/****** API CALLS***** */
 
 //CARD SECTION
 const opts = {
@@ -134,17 +42,54 @@ api.getInitialCards()
 		console.error(`Error getting cards: ${err} `);
 	});
 
-/************************************************ */
 
-/**USER ROUTES
- * 1. UPDATE USER ROUTE
+	api.getUserInfo()
+	.then((res) => {
+		userInfo.setUserInfo({
+			name: res.name,
+			description: res.about,
+		});
+	})
+	.catch((err) => {
+		console.log("ERROR: ", err);
+		console.error(err);
+	});
+
+/**
+ * Param: userData
+ * Description:	Sets the change/edit name/title and the description of the profile
+ * 					UPDATE USER PROFILE INFO ROUTE
  */
+function handleProfileSubmitForm(userData) {
+	// Call the updateUserInfo method from the Api class
+	api.updateUserInfo(userData.title, userData.description)
+		.then(() => {
+			userInfo.setUserInfo({
+				name: userData.title,
+				description: userData.description,
+			});
+			profileEditForm.close();
+		})
+		.catch((err) => {
+			console.error(`Error updating new information: ${err}`);
+		});
+} //end func
 
-/************************************************ */
 
-//POP UP WITH IMAGE
-const imagePopup = new PopupWithImage(utils.htmlIds.imagePreviewPopup);
-imagePopup.setEventListeners();
+function handleDeleteCard(card){
+	/**deleteCardModal open -- need to create HTML element like modal_opened */
+	api.deleteCard(card).then( () => {
+		//deleteCardModal close;
+		card.remove();
+
+	}).catch( (err) => {
+		console.log(err);
+		console.error(`Error removing Card: ${err}`);
+	});
+
+}//end func
+
+/**__________________________________________ */
 
 //EDIT PROFILE FORM
 const profileEditForm = new PopupWithForm(
@@ -160,12 +105,6 @@ utils.profileEditButton.addEventListener("click", () => {
 	profileEditForm.open();
 });
 
-//USER INFO
-const userInfo = new UserInfo({
-	titleSelector: utils.htmlIds.profileTitle,
-	descriptionSelector: utils.htmlIds.profileDescription,
-});
-
 //ADD CARD FORM
 const addCardForm = new PopupWithForm(
 	utils.htmlIds.addCardPopup,
@@ -175,4 +114,92 @@ addCardForm.setEventListeners();
 
 utils.profileAddCardButton.addEventListener("click", () => {
 	addCardForm.open();
+});
+
+//POP UP WITH IMAGE
+const imagePopup = new PopupWithImage(utils.htmlIds.imagePreviewPopup);
+imagePopup.setEventListeners();
+
+/**
+ * Param: cardData
+ * Description:	card Data is used that is passed to the Image form for display
+ */
+//Card Image Preview
+function handleImageClick(cardData) {
+	imagePopup.open(cardData);
+} //end func
+
+const deleteCardPopup = 
+
+
+/**
+ * param: data
+ * Description:	Takes a data object that is used to create a card with the Card object
+ * 						Card Element is created and passed along to the Section object
+ * 						Section object visually puts the card on the screen
+ */
+//func to make new card
+function createCard(data) {
+	const newCard = new Card(
+		data,
+		utils.htmlIds.cardTemplate,
+		handleImageClick,
+	); //pass new info to new card
+	const elementCard = newCard.getCard();
+	sectionCards.addItems(elementCard);
+} //end func
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Param: settings/configurations
+ * Description: all form validation initialize
+ */
+function validateForms(opts) {
+	//initialize forms
+	const forms = document.querySelectorAll(utils.config.formSelector);
+	const formArray = Array.from(forms);
+	formArray.forEach((form) => {
+		const validateForm = new FormValidator(opts, form);
+		validateForm.enableValidation();
+	});
+} //end func
+
+/**
+ * Description:	Adds a new card everytime the + button is clicked
+ */
+function handleAddCardSubmitForm() {
+	//add new card i.e. Create Card
+	const name = utils.newCardTitleInput.value; //newCardTitleValue
+	const link = utils.newCardUrlInput.value; //newCardUrlValue
+	const dataCard = { name, link }; //collect new card data and link
+	createCard(dataCard);
+
+	//close addCardPopup
+	addCardForm.close();
+} //end func
+
+/********************INSTANISATIONS BELOW******************** */
+
+//ALL FORMS VALIDATIONS
+validateForms(utils.config);
+
+//USER INFO
+const userInfo = new UserInfo({
+	titleSelector: utils.htmlIds.profileTitle,
+	descriptionSelector: utils.htmlIds.profileDescription,
 });
